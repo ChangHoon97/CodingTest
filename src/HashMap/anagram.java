626e6005535e15bec356115cd4faf7000269e630
ChangHoon <hyo04044@naver.com>
2023-08-10 17:50:25 +0900

    anagram

---
+++ src/HashMap/anagram.java
@@ -0,0 +1,32 @@
+package HashMap;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+//아나그램
+//AbaAeCe
+//baeeACA
+
+public class anagram {
+    public String solution(String str1, String str2){
+        String answer = "YES";
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(char x : str1.toCharArray()){
+            map.put(x,map.getOrDefault(x,0)+1);
+        }
+        for(char x : str2.toCharArray()){
+            if(!map.containsKey(x) || map.get(x)==0) return "NO";
+            map.put(x,map.get(x)-1);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        anagram T = new anagram();
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+        String str2 = sc.next();
+        System.out.println(T.solution(str1,str2));
+    }
+
+}
