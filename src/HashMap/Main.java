79d34fd8b6f31cfdc8cb2f19ab1bea6060a15462
Hoon <hyo04044@naver.com>
2023-09-21 16:27:14 +0900

    create : String test

--- src/HashMap/Main.java
+++ src/HashMap/Main.java
@@ -2,7 +2,17 @@ package HashMap;
 
 public class Main {
 
+    public void solution(String s){
+        char[] arr = s.toCharArray();
+        for(int i =0; i< arr.length; i++){
+            System.out.println(arr[i]);
+        }
+
+    }
     public static void main(String[] args) {
+        Main T = new Main();
+        String s = "abce";
+        T.solution(s);
 
     }
 }
